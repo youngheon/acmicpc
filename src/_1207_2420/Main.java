27b37247900c5e172051de9d1c50b3d54678a511
Young-heon <youngheon88@gmail.com>
2015-12-08 00:28:40 +0900

    2420 big Integer solve

---
+++ src/_1207_2420/Main.java
@@ -0,0 +1,18 @@
+package _1207_2420;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String [] str2 = str.split("\\s+");
+		BigInteger a = new BigInteger(str2[0]); 
+		BigInteger b = new BigInteger(str2[1]); 
+		
+		
+		System.out.println(a.subtract(b).abs());
+	}
+}
--- src/_1207_2555/Main.java
+++ src/_1207_2555/Main.java
@@ -2,10 +2,12 @@ package _1207_2555;
 
 public class Main {
 	public static void main(String [] args){
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  )");
-		System.out.println(" \\(__)|");
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
 	}
 }
 
