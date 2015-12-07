b8fd7bfcce92954e1c64ee8250c48981712a8c27
Choi Young Heon <Young-heon@Choiui-MacBook-Air.local>
2015-12-07 14:44:57 +0900

    10817 solve

---
+++ src/_1207_10817/Main.java
@@ -0,0 +1,22 @@
+package _1207_10817;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String [] str2 = str.split("\\s+");
+		int [] array = new int[3];
+		array[0] = Integer.parseInt(str2[0]);
+		array[1] = Integer.parseInt(str2[1]);
+		array[2] = Integer.parseInt(str2[2]);
+		
+		Arrays.sort(array);
+		
+		System.out.println(array[1]);
+		
+	}
+
+}
