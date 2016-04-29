f4f067e49e92e671b92e2eace1d97e1aa1403f5b
Young-heon <youngheon88@gmail.com>
2016-04-30 00:02:25 +0900

    Solve basic Math problem

--- src/_10871/Main.java
+++ src/_10871/Main.java
@@ -1,10 +1,28 @@
 package _10871;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String str2 = sc.nextLine();
+		
+		String [] sizeValue = str.split("\\s+");
+		String [] values = str2.split("\\s+");
+		
+		int numSize = Integer.parseInt(sizeValue[0]);
+		int value = Integer.parseInt(sizeValue[1]);
+		
+		int [] num = new int[values.length];
+		for(int i=0;i<values.length;i++){
+			num[i]=Integer.parseInt(values[i]);
+			if(value>num[i]){
+				System.out.print(num[i]+" ");
+			}
+		}
+		
 	}
 
 }
--- src/_1212/Main.java
+++ src/_1212/Main.java
@@ -1,76 +1,34 @@
 package _1212;
 
-import java.math.BigInteger;
 import java.util.Scanner;
-
+//2진수 to 8진
 public class Main {
 
-	public static int func(int n){
-		if(n==0){
-			return 0; 
-		}
-		return (n%10)+func(n/10)+func(n/100);
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		String str= sc.nextLine();
+		System.out.print(octalToBinary(str));
+		
 	}
-	
-	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		String str = sc.next();
-//		BigInteger.valueOf(Long.parseLong(str,8));
-//		
-//		
-//		System.out.println(BigInteger.toString(BigInteger.valueOf(Integer.parseInt(str,8))));
-		
-		
-//	int a = 1<<2;
-//	int b = 1<<3;
-//	int c = 1<<1;
-//	int result = 7;
-//	
-//	result &=a;
-//	result |=b;
-//	result ^=c;
-//	result = ~result;
-//	System.out.println(result);
-//		int j;
-//		for(int i=1;i<=4;i++){
-//			for(j=i;j<=4;j++){
-//				System.out.print(j);
-//			}
-//			System.out.println();
-		
-//		}
+	static String octalToBinary(String s) {
+        char int8[] = s.toCharArray();
+        int n = int8.length;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String a = Integer.toBinaryString((int) int8[i] - 48);
+            if ((n - i) < n) a = radix(a);
+            sb.append(a);
+        }
+       return sb.toString();
+ }
+static String radix(String r) {
+       int n = r.length();
+       if (n < 3) {
+          r = "00" + r;
+          n = r.length();
+          return r.substring(n - 3, n);
+      } else
+          return r;
 
-
-//
-//		System.out.println(func());
-//		System.out.println(func(512));
-//		System.out.println(func(1049));
-//		System.out.println(func(2013));
-//	System.out.println(func(3021));
-//		
-//int n=63, c=0;
-//		while(n>1){
-//			c+=(n&3);
-//			n/=2;
-//			
-//			
-//		}
-//		System.out.println(c);
-int a=1;
-int b=20;
-int ret=0;
-for(int i=a;i<=b;++i){
-	Boolean ok = true;
-	if(i==1) ok =false;
-	for(int j=2;j*j<=i;++j){
-		if(i%j==0){
-			ok =false;
-		}
-	}
-	if(ok) ret++;
 }
-System.out.println(ret);
-		
-		
-	}
 }
