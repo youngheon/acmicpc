610f96cd117abb31aaad3e1dfb8c1f741e3e8ffc
Young-heon <youngheon88@gmail.com>
2016-05-12 23:19:15 +0900

    solve 11557

--- src/_11557/Main.java
+++ src/_11557/Main.java
@@ -3,30 +3,71 @@ package _11557;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) {
-
-		Map<String,Integer> unvi = new HashMap<String,Integer>();
-		Scanner sc = new Scanner(System.in );
-		int testCase = sc.nextInt();
-		for(int i=0;i<testCase;i++){
-			int univNum = sc.nextInt();
-			
-			//StringTokenizer st = new StringTokenizer(str," ");
-			//System.out.print(st.countTokens());
-			
-			
-			for(int j=0; j<univNum; j++){
-				String str = sc.next();
-				String [] str2 = str.split(""));
-				unvi.put(st.nextToken(), Integer.parseInt(st.nextToken()));
-				
-			}
-			
-		}
+	
+	Scanner sc;
+	Map<String,Integer> univ; 
+	private int testcase;
+	private int univNum;
+	
+	public void setTestcase(int testcase){
+		this.testcase = testcase;
 	}
-
+	
+	public int getTestcase(){
+		return this.testcase;
+	}
+	
+	public void setUnivNum(int univNum){
+		this.univNum = univNum;
+	}
+	
+	public int getUnivNum(){
+		return this.univNum;
+	}
+	
+	public Main(){
+		sc = new Scanner(System.in);
+		univ = new HashMap<String,Integer>();
+		setTestcase(sc.nextInt());
+	}
+	
+	public Map<String, Integer> input(){
+		setUnivNum(sc.nextInt());
+		
+		
+		for(int i=0; i<getUnivNum(); i++){
+			univ.put(sc.next(), Integer.parseInt(sc.next()));
+		}
+		return univ;
+	}
+	
+	public void calcPrint(Map<String,Integer> univ){
+		Set<String> set = univ.keySet();
+		int num = 0;
+		String univName="";
+		for(String keys : set){
+			String key = keys;
+			int value = univ.get(key);
+			if(num < value){
+				num = value;
+				univName = key;
+			}
+		}
+		System.out.println(univName);
+		univ.clear();
+	}
+	
+	public void run(){
+		for(int i=0;i<getTestcase();i++)
+			calcPrint(input());
+	}
+	
+	public static void main(String[] args) {
+		Main m = new Main();
+		m.run();
+	}
 }
